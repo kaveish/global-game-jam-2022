43632a5063d663a0d3a9e1bb3aaa285c0384b486
Tom H <tomhands@physik.uzh.ch>
2022-01-30 13:52:02 +0100

    Random audio update, enemies chase player

--- Assets/Scripts/RandomAudioCall.cs
+++ Assets/Scripts/RandomAudioCall.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class RandomAudioCall : MonoBehaviour
 {
-    public AudioSource myCall;
+    //public AudioSource myCall;
+    public List<AudioSource> calls = new List<AudioSource>();
     public float minimum_delay = 3.5f;
     public float maximum_delay = 10.5f;
     private float last_fire = 0.0f;
     private float delay = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,9 @@ public class RandomAudioCall : MonoBehaviour
     {
         if (Time.time > last_fire + delay)
         {
-            myCall.Play();
+            //myCall.Play();
+            int index = Random.Range(0,calls.Count);
+            calls[index].Play();
             last_fire = Time.time;
             delay = Random.Range(minimum_delay, maximum_delay);
         }
